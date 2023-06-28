2b30eef789e24e6d29aac4864b1501a6f5983b1f
arunkumarps828 <arunkumarps828@gmail.com>
2023-06-28 17:25:45 +0530

    content changes

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,23 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap"
           rel="stylesheet"
         />
+        <meta name="title" content="Winnopro: Winning through Innovation" />
+        <meta
+          name="description"
+          content="Winnopro Technologies Pvt Ltd. is committed to improve the quality of lives through Innovation. Join us in leading the new-gen Customer Experience across Digital Media platforms including Web, Mobile and Social media. We are in a mission to bring new dimensions to improve and enhance the customer engagement and experience."
+        />
+        <meta
+          name="abstract"
+          content="We are Design Thinking and Innovative Minded group of people, working towards a Mission to Improve the Quality of Lives!"
+        />
+        <meta
+          name="keywords"
+          content="Winnopro, Innovation, Innovative, Customer Experience, Customer Journey, Design Thinking, Quality of Lives, Improving Lives, User Story, User Experience, Closed Network, Job Portal, Recruitment Portal, Mobile App, Artificial Intelligence, Machine Learning"
+        />
+        <meta
+          name="google-site-verification"
+          content="E2-H9L-WrEa39xLSbpDVODykJ7jkK97ZT7O6s3HP4JM"
+        />
       </Head>
       <body className=" scrollbar font-cambria ">{children}</body>
     </html>
--- sections/Team/Team.js
+++ sections/Team/Team.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 import image from "../../public/images/teamBg.png";
 import dpHanish from "../../public/images/TeamMembers/Image 9.png";
 import dpDeepti from "../../public/images/TeamMembers/Image 10.png";
@@ -13,64 +12,34 @@ import dpArunTeja from "../../public/images/TeamMembers/dpArunTeja.jpeg";
 import CustomHeader from "../../components/Customheader/CustomHeader";
 import Image from "next/image";
 import linkedIn from "../../public/Icons/linkedin.svg";
-import Slider from "react-slick";
 
 const Team = () => {
-  const slider = React.useRef(null);
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    customPaging: function (i) {
-      return (
-        <div key={i} className="dot">
-          {i}
-        </div>
-      );
-    },
-    dotsClass: "slick-dots slick-thumb",
-  };
-
   const data = [
-    [
-      {
-        name: "Hanish Keloth",
-        detail: "CEO",
-        dp: dpHanish,
-        linkedin: "https://www.linkedin.com/in/hanishkeloth",
-      },
-      {
-        name: "Arunteja Bukkaparapu",
-        detail: "COO",
-        dp: dpArunTeja,
-        linkedin: "https://www.linkedin.com/in/aruntejabukkaparapu/",
-      },
-      {
-        name: "Siraj Shaukat Ali",
-        detail: "VP-Marketing",
-        dp: dpSiraj,
-        linkedin: "https://www.linkedin.com/in/siraj-shaukat-ali-7aa58018",
-      },
-      {
-        name: "Ubaid Shaik",
-        detail: "Business Head",
-        dp: dpUbaid,
-        linkedin: "https://www.linkedin.com/in/ubaidshaikh",
-      },
-    ],
-    [
-      {
-        name: "Deepthi Mayilat",
-        detail: "Business Head",
-        dp: dpDeepti,
-        linkedin: "https://www.linkedin.com/in/deepthimayilat",
-      },
-    ],
+    {
+      name: "Hanish Keloth",
+      detail: "CEO",
+      dp: dpHanish,
+      linkedin: "https://www.linkedin.com/in/hanishkeloth",
+    },
+    {
+      name: "Arunteja Bukkaparapu",
+      detail: "COO",
+      dp: dpArunTeja,
+      linkedin: "https://www.linkedin.com/in/aruntejabukkaparapu/",
+    },
+    {
+      name: "Siraj Shaukat Ali",
+      detail: "VP-Marketing",
+      dp: dpSiraj,
+      linkedin: "https://www.linkedin.com/in/siraj-shaukat-ali-7aa58018",
+    },
+
+    {
+      name: "Deepthi Mayilat",
+      detail: "Business Head",
+      dp: dpDeepti,
+      linkedin: "https://www.linkedin.com/in/deepthimayilat",
+    },
   ];
   const sectionRefs = useRef([]);
 
@@ -134,66 +103,52 @@ const Team = () => {
       <div className=" px-4 py-8 sm:px-6 sm:py-12 lg:px-24 lg:py-14 font-cambria ">
         <CustomHeader sub="TEAM" main="Our Team" />
 
-        <Slider ref={slider} {...settings}>
-          {Object.keys(data).map((item, i) => (
-            <div
-              onWheel={(event) => {
-                if (event.nativeEvent.wheelDelta > 0) {
-                  slider?.current?.slickNext();
-                } else {
-                  slider?.current?.slickPrev();
-                }
-              }}
-            >
-              <div
-                ref={(ref) => (sectionRefs.current[0] = ref)}
-                id="team-wrapper"
-                className="mt-8 grid grid-cols-2 gap-3 md:mt-10 md:grid-cols-3 md:gap-8 xxl:items-center  lg:grid-cols-3 xl:grid-cols-4 "
-              >
-                {data[item].map((i, index) => {
-                  return (
-                    <div className=" h-max " id="member">
-                      <div
-                        ref={(ref) => (sectionRefs.current[1] = ref)}
-                        className=" w-full h-full gap-2 "
-                      >
-                        <div
-                          className={`${
-                            index % 2 !== 0 ? "bg-sec-blue" : " bg-main-orange "
-                          }  w-12/12 h-full   `}
-                        >
-                          <div
-                            // className=" lg:bg-cover lg:bg-no-repeat lg:bg-center lg:h-[120vh]"
-                            className="bg-green h-10/12 w-10/12 sm:h-9/12 sm:w-9/12 top-2 left-2 sm:top-4 sm:left-4 relative "
-                          >
-                            <Image
-                              className="w-full h-full object-cover "
-                              src={i.dp}
-                            />
-                          </div>
-                        </div>
-                      </div>
-                      <div className="flex flex-col h-full items-center py-6 gap-2 rounded-xl ">
-                        <h2 className=" text-[16px] lg:text-card text-center text-wp-gray font-bold">
-                          {i.name}
-                        </h2>
-                        <h2 className=" text-[14px] lg:text-[20px]  text-wp-gray font-medium">
-                          {i.detail}
-                        </h2>
-                        <a
-                          href={i.linkedin}
-                          className="shrink-0 mt-2 sm:block lg:block rounded-full w-16 items-center h-16 bg-blue p-0"
-                        >
-                          <Image className="w-full" src={linkedIn} />
-                        </a>
-                      </div>
+        <div
+          ref={(ref) => (sectionRefs.current[0] = ref)}
+          id="team-wrapper"
+          className="mt-8 grid grid-cols-2 gap-3 md:mt-10 md:grid-cols-3 md:gap-8 xxl:items-center  lg:grid-cols-3 xl:grid-cols-4 "
+        >
+          {data.map((i, index) => {
+            return (
+              <div className=" h-max " id="member">
+                <div
+                  ref={(ref) => (sectionRefs.current[1] = ref)}
+                  className=" w-full h-full gap-2 "
+                >
+                  <div
+                    className={`${
+                      index % 2 !== 0 ? "bg-sec-blue" : " bg-main-orange "
+                    }  w-12/12 h-full   `}
+                  >
+                    <div
+                      // className=" lg:bg-cover lg:bg-no-repeat lg:bg-center lg:h-[120vh]"
+                      className="bg-green h-10/12 w-10/12 sm:h-9/12 sm:w-9/12 top-2 left-2 sm:top-4 sm:left-4 relative "
+                    >
+                      <Image
+                        className="w-full h-full object-cover "
+                        src={i.dp}
+                      />
                     </div>
-                  );
-                })}
+                  </div>
+                </div>
+                <div className="flex flex-col h-full items-center py-6 gap-2 rounded-xl ">
+                  <h2 className=" text-[16px] lg:text-card text-center text-wp-gray font-bold">
+                    {i.name}
+                  </h2>
+                  <h2 className=" text-[14px] lg:text-[20px]  text-wp-gray font-medium">
+                    {i.detail}
+                  </h2>
+                  <a
+                    href={i.linkedin}
+                    className="shrink-0 mt-2 sm:block lg:block rounded-full w-16 items-center h-16 bg-blue p-0"
+                  >
+                    <Image className="w-full" src={linkedIn} />
+                  </a>
+                </div>
               </div>
-            </div>
-          ))}
-        </Slider>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
